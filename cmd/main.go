284ec8da5988418802b65d1e6062f5125b5c8749
Tran Hoang <hoangtranquang@admicro.vn>
2020-03-27 15:49:52 +0700

    Add convert int32 to bytes

--- cmd/main.go
+++ cmd/main.go
@@ -6,6 +6,11 @@ import (
 )
 
 func main()  {
-	m3_128 := murmur3.HashString(-1467523828, "681236075540516864")
-	fmt.Println(m3_128.AsInt())
+	//m3_128 := murmur3.HashString(-1467523828, "681236075540516864")
+	//fmt.Println(m3_128.AsInt())
+
+	bytes := murmur3.ToBytes(-1239833368)
+
+	fmt.Println(bytes)
+
 }
--- function_common.go
+++ function_common.go
@@ -14,3 +14,23 @@ package murmur3
 //		log.Fatalf("[E] %v %s:%d", err, fn, line)
 //	}
 //}
+
+func ToBytes(val int) []byte {
+	b := make([]byte, 8)
+
+	for i := 7; i > 0; i-- {
+		b[i] = byte(val)
+		val = RightShift(val, 8)
+	}
+
+	b[0] = byte(val)
+	return b
+}
+
+func RightShift(x int, k int) int {
+	if x >= 0 {
+		return x>>k
+	} else {
+		return ( x >> k) + (int(2) << (63-k))
+	}
+}
